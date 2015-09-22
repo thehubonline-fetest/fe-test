1cfe9bdefef8427f3c8e53add94e10a7478716f9
Pieter <pieter.van.hulle@gmail.com>
2015-09-22 13:58:26 +0200

    second commit

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : 'scss/style.scss',
+          'css/styles.css' : 'scss/styles.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    cssmin: {
+          css:{
+              src: 'css/styles.css',
+              dest: 'css/styles.min.css'
+          }
+    },
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default',['watch','cssmin']);
+}
